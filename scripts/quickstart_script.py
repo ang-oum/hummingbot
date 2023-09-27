a6f26247b857f6a3cb8f03ec79b85096349eeb15
Angelos <89702737+ang-oum@users.noreply.github.com>
2023-09-27 10:22:54 +0200

    .

--- scripts/quickstart_script.py
+++ scripts/quickstart_script.py
@@ -4,7 +4,7 @@ from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 class QuickstartScript(ScriptStrategyBase): 
     markets = {"binance_paper_trade":{"BTC-USDT, ETH-USDT"}}
 
-    def in_tick(self):
+    def on_tick(self):
         price = self.connectors["binance_paper_trade"].get_mid_price("BTC-USDT")
         msg = f"Bitcoin price ${price}"
         self.logger().info(msg)                  #insert logger example: telegram
